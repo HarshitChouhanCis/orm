140f6e9200005337e6c1cb77de9ce04ea327c127
harshit c <harshit.c@cisinlabs.com>
2025-06-23 17:17:34 +0530

    modification on setup.js

--- setup.js
+++ setup.js
@@ -10,4 +10,4 @@ DATABASE_URL="postgresql://postgres:xxxxxx@localhost:5432/prismaDB?schema=public
 
 
 
-// add thr above code in .env files and then your credentials("please enter db name in place of `postgres` and enter password in place of `xxxxxx`")
+// add thr above code in .env files and then your credentials("please enter db name in place of `postgres` and enter password in place of `xxxxxx`" and last pass name in place of `prismaDB`)
